91831b89c6793068ebe520aa1dd1d71db40bf8dd
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-06-07 23:42:54 +0300

    Create WithdrawSilver.js

---
+++ WithdrawSilver.js
@@ -0,0 +1,8 @@
+let wTotal = 0;
+for (planet of df.getMyPlanets().filter(p => p.planetType === 3)){
+    if (planet.silver/planet.silverCap >= 0.5){
+        df.withdrawSilver(planet.locationId, Math.floor(planet.silver))
+		wTotal += Math.floor(planet.silver);
+    }
+}
+console.log("Total silver withdrawed: ", wTotal);
