f20ea0198552863a51d6824b5604e555a5226551
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-06-08 23:41:36 +0300

    Create MoveAllArtifactsToSR.js

---
+++ MoveAllArtifactsToSR.js
@@ -0,0 +1,15 @@
+//Move All artifacts from All planets towards Space Rifts
+let wArt = 0;
+for(planet of df.getMyPlanets().filter(p => p.planetType === 3).filter(p => p.heldArtifactIds.length > 0)){
+	for (i of planet.heldArtifactIds){
+		console.log(planet.locationId, i)
+		if(planet.planetLevel > df.getArtifactWithId(i).rarity){
+			df.withdrawArtifact(planet.locationId, i);
+			wArt++;
+		}
+		else {
+		 console.log("Space rift LvL too small: ", planet.planetLevel, df.getArtifactWithId(i).rarity);
+		}
+	}
+}
+console.log("Withdrawed artfacts: ", wArt);
