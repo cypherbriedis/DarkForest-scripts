d220f0c73a0810ff754ef1a866d44d015afed3b7
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-06-08 23:39:34 +0300

    Update WithdrawArtifact.js

--- WithdrawArtifact.js
+++ WithdrawArtifact.js
@@ -1,9 +1,14 @@
 let wArt = 0;
 for(planet of df.getMyPlanets().filter(p => p.planetType === 3).filter(p => p.heldArtifactIds.length > 0)){
 	for (i of planet.heldArtifactIds){
-		//console.log(planet.locationId, "-", i)
-		df.withdrawArtifact(planet.locationId, i);
-		wArt++;
+		console.log(planet.locationId, i)
+		if(planet.planetLevel > df.getArtifactWithId(i).rarity){
+			df.withdrawArtifact(planet.locationId, i);
+			wArt++;
+		}
+		else {
+		 console.log("Space rift LvL too small: ", planet.planetLevel, df.getArtifactWithId(i).rarity);
+		}
 	}
 }
 console.log("Withdrawed artfacts: ", wArt);
