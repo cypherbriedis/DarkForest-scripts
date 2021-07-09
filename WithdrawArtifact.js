0a3c8dedbdcc1e17ab3e156245a27c23f0fa3f60
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-07-09 15:55:51 +0300

    Update WithdrawArtifact.js

--- WithdrawArtifact.js
+++ WithdrawArtifact.js
@@ -2,7 +2,7 @@ let wArt = 0;
 for(planet of df.getMyPlanets().filter(p => p.planetType === 3).filter(p => p.heldArtifactIds.length > 0)){
 	for (i of planet.heldArtifactIds){
 		console.log(planet.locationId, i)
-		if(planet.planetLevel > df.getArtifactWithId(i).rarity){
+		if(planet.planetLevel >= df.getArtifactWithId(i).rarity){
 			df.withdrawArtifact(planet.locationId, i);
 			wArt++;
 		}
