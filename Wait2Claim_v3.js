30e72213d6c2ed4f724bdd2cfbb3e3ffea452f39
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-08-22 14:37:01 +0300

    Update Wait2Claim_v3.js

--- Wait2Claim_v3.js
+++ Wait2Claim_v3.js
@@ -3,26 +3,29 @@ function sleep(ms) {
 }
 
 async function checkClaimer() {
-	let planetId = "000045d202a2d07ef027b6fef565e4636355bf0e6b2e8841c7e23afa07f4c093";    //planetID of planet to clime
+	let planetId = "0000b67f02f46ae73cf99a6ddb11cbc42a949895410afa2fb0192b185814274c";		//planetID of planet to clime
 	let myId = df.account;
 	let planetClaimer = df.getPlanetWithId(planetId);
+	let printTerminal = 20;																	//Print to terminal each 10min
 	while(planetClaimer.claimer === undefined){
-		var d = new Date();
 		planetClaimer = df.getPlanetWithId(planetId);
-		let voyage = df.getAllVoyages().filter(p=>(p.player === df.account && p.toPlanet === planetId));
-		let trTime = voyage[0].arrivalTime * 1000;
-		let secLeft = trTime - Date.now();
-		let enLeft = Math.floor(voyage[0].energyArriving - planetClaimer.energy * planetClaimer.defense/100);
-		let inpStr = "Ok";
-		if (enLeft <=0){
-			inpStr = "No energy";
+		if(printTerminal === 0){
+			var d = new Date();
+			let voyage = df.getAllVoyages().filter(p=>(p.player === df.account && p.toPlanet === planetId));
+			let trTime = voyage[0].arrivalTime * 1000;
+			let secLeft = trTime - Date.now();
+			let enLeft = Math.floor(voyage[0].energyArriving - (planetClaimer.energy * planetClaimer.defense / 100));
+			let inpStr = "Ok";
+			if (enLeft <=0){
+				inpStr = "No energy";
+			}
+			df.terminal.current.printShellLn(d.toTimeString().split(' ')[0] + " " + inpStr + " " + secLeft + "s " + enLeft);
+			printTerminal = 20;
 		}
-			
-		df.terminal.current.printShellLn(d.toTimeString().split(' ')[0] + " " + inpStr + " " + secLeft + "s " + enLeft);
 		if(planetClaimer.owner === myId && planetClaimer.claimer === undefined){
-			
-				df.claimLocation(planetId);
+			df.claimLocation(planetId);
 		}
+		printTerminal--;
 		await sleep(30000);
 	}
 	if(planetClaimer.claimer === myId){
@@ -35,7 +38,7 @@ async function checkClaimer() {
 async function claimPlanet() {
 	let startSnark = 2000;																	//Start generating contract 2sec before arriving. Depends on how fast is PC
 	let planetFrom = "000066230001a179beb7b2086e3fd5ded221beca52c94af9780f22164dec7067";	//Change to planet ID you're going FROM
-	let planetTo = "000045d202a2d07ef027b6fef565e4636355bf0e6b2e8841c7e23afa07f4c093";		// Change to planet ID you're going TO
+	let planetTo = "0000b67f02f46ae73cf99a6ddb11cbc42a949895410afa2fb0192b185814274c";		// Change to planet ID you're going TO
 	let travelTime = df.getTimeForMove(planetFrom, planetTo);								//Just for information
 	df.terminal.current.printShellLn("Travel time: " + travelTime);							//Just for information
 	let voyage = df.getAllVoyages().filter(p=>(p.player === df.account && p.toPlanet === planetTo));
