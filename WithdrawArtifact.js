7b8b88901d102f2f8219fbd8e1caf0ef75789aa3
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-10-04 13:22:00 +0300

    Update WithdrawArtifact.js

--- WithdrawArtifact.js
+++ WithdrawArtifact.js
@@ -1,4 +1,4 @@
-let wArt = 0;
+wArt = 0;
 for(planet of df.getMyPlanets().filter(p => p.planetType === 3).filter(p => p.heldArtifactIds.length > 0)){
 	for (i of planet.heldArtifactIds){
 		console.log(planet.locationId, i)
