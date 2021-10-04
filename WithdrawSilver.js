2e8678d5d00cfc86883a6e1d53d22c89349d3513
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2021-10-04 12:50:09 +0300

    Update WithdrawSilver.js

--- WithdrawSilver.js
+++ WithdrawSilver.js
@@ -1,4 +1,4 @@
-let wTotal = 0;
+wTotal = 0;
 for (planet of df.getMyPlanets().filter(p => p.planetType === 3)){
     if (planet.silver/planet.silverCap >= 0.5){
         df.withdrawSilver(planet.locationId, Math.floor(planet.silver))
