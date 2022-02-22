2076d1ce6a8ebf59451781027fdff9eeab182073
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2022-02-23 00:51:27 +0200

    Update InvadeAndCapture.js

--- InvadeAndCapture.js
+++ InvadeAndCapture.js
@@ -13,6 +13,9 @@ async function capturePlanet() {
 	loop = true;
 	myAccount = df.account;
 	while(loop){
+		invadingPlan = 0;
+		capturePln = 0;
+		waitingPln = 0;
 		captureZones = df.getCaptureZones();
 		candidates = df.getMyPlanets().filter(p=>p.invader == "0x0000000000000000000000000000000000000000");
 		for (cZ = captureZones.values(), val=null; val=cZ.next().value;){
@@ -23,24 +26,30 @@ async function capturePlanet() {
 				if(check_a_point(planet.location.coords.x, planet.location.coords.y, xCoord, yCoord, radiusR)){
 					df.terminal.current.printShellLn("Invading planet: " + planet.locationId);
 					df.invadePlanet(planet.locationId);
+					invadingPlan++;
 				}
 			}
 		}
-		planets = df.getMyPlanets().filter(p=>p.capturer == "0x0000000000000000000000000000000000000000" && p.invader == myAccount)
+		planets = df.getMyPlanets().filter(p=>p.capturer == "0x0000000000000000000000000000000000000000" && p.invader == myAccount);
 		for(planet of planets){
 			
 			if(planet.invadeStartBlock + 2100 < df.getEthConnection().blockNumber && planet.energy/planet.energyCap*100 >= 81){
 				df.terminal.current.printShellLn("Requesting:" + planet.locationId);
 				df.capturePlanet(planet.locationId);
+				capturePln++;
 			}
 			else
 			{
 				blocksLeft = planet.invadeStartBlock + 2050 - df.getEthConnection().blockNumber;
 				energyProc = Math.ceil(planet.energy/planet.energyCap*100);
 				df.terminal.current.printShellLn("Blocks Left: " + blocksLeft + " Energy: " + energyProc + "% LocationId: " + planet.locationId);
+				waitingPln++;
 			}
 		}
-		df.terminal.current.printShellLn("************************")
+		df.terminal.current.printShellLn("Invading Planets:  " + invadingPlan);
+		df.terminal.current.printShellLn("Capturing Planets: " + capturePln);
+		df.terminal.current.printShellLn("Waiting2Capture:   " + waitingPln);
+		df.terminal.current.printShellLn("#######################");
 		await sleep(60000);
 	}
 }
