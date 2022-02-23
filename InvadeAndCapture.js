1f7f0ae6603cef1e0d204cada5dba1369847ee44
cypherbriedis <47241384+cypherbriedis@users.noreply.github.com>
2022-02-23 16:27:58 +0200

    Update InvadeAndCapture.js

--- InvadeAndCapture.js
+++ InvadeAndCapture.js
@@ -38,7 +38,7 @@ async function capturePlanet() {
 		planets = df.getMyPlanets().filter(p=>p.capturer == "0x0000000000000000000000000000000000000000" && p.invader == myAccount);
 		for(planet of planets){
 			
-			if(planet.invadeStartBlock + 2100 < df.getEthConnection().blockNumber && planet.energy/planet.energyCap*100 >= 81){
+			if(planet.invadeStartBlock + 2050 < df.getEthConnection().blockNumber && planet.energy/planet.energyCap*100 >= 81){
 				df.terminal.current.printShellLn("Capture: {x: " + planet.location.coords.x + ",y: " + planet.location.coords.y+"}");
 				df.capturePlanet(planet.locationId);
 				
